42e6697250d28e6d5533aadb7c72a1bbd3b176af
Alexandre da Silva Barbosa <alexandrebarbosa8371@gmail.com>
2023-05-23 11:17:38 -0300

    Estudo sobre Tuples

---
+++ modulo-02/18-aula-tipo-tuple/tupleType.js
@@ -0,0 +1,16 @@
+"use strict";
+// ==> Exemplo 01 - Uso simples de Tuplas em TypeScript
+let pessoa;
+pessoa = ['Alexandre Barbosa', 'Back-end Web Developer', 21];
+// pessoa = [21, 'Alexandre Barbosa', 'Back-end Web Developer'];
+console.log(pessoa);
+// ==> Exemplo 02 - Acessando o valor da Tupla
+let pessoa1;
+pessoa1 = ['Alexandre Barbosa', 'Back-end Web Developer', 21];
+console.log(pessoa1[1]);
+// ==> Exemplo 03 - Outra forma de usar Tuplas em TypeScript (com labels)
+let listaFrutas = ['Abacaxi', 'Laranja', 'Maçã', 'Melancia', 'Morango'];
+// ==> Exemplo 04 - Usando Tuplas com Spread Operator
+// ==> Exemplo 05 - Lista Heterogênea de Tupla
+// ==> Exemplo 06 - Uso de função com Tuplas
+// ==> Exemplo 07 - Labeled Tuples com Spread Operator numa função
---
+++ modulo-02/18-aula-tipo-tuple/tupleType.ts
@@ -0,0 +1,26 @@
+// ==> Exemplo 01 - Uso simples de Tuplas em TypeScript
+
+let pessoa: [string, string, number];
+pessoa = ['Alexandre Barbosa', 'Back-end Web Developer', 21];
+// pessoa = [21, 'Alexandre Barbosa', 'Back-end Web Developer'];
+
+console.log(pessoa);
+
+// ==> Exemplo 02 - Acessando o valor da Tupla
+
+let pessoa1: [string, string, number];
+pessoa1 = ['Alexandre Barbosa', 'Back-end Web Developer', 21];
+
+console.log(pessoa1[1]);
+
+// ==> Exemplo 03 - Outra forma de usar Tuplas em TypeScript (com labels)
+
+let listaFrutas: [string, ...string[]] = ['Abacaxi', 'Laranja', 'Maçã', 'Melancia', 'Morango'];
+
+// ==> Exemplo 04 - Usando Tuplas com Spread Operator
+
+// ==> Exemplo 05 - Lista Heterogênea de Tupla
+
+// ==> Exemplo 06 - Uso de função com Tuplas
+
+// ==> Exemplo 07 - Labeled Tuples com Spread Operator numa função
